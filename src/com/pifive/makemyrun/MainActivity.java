574aa8e3040b9c0b9291ceb74bcf4381f8fefd97
Anton Lindgren <lindgren.anton@gmail.com>
2012-09-22 10:48:43 +0200

    Hello Maps

---
+++ src/com/pifive/makemyrun/MainActivity.java
@@ -0,0 +1,31 @@
+package com.pifive.makemyrun;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+import com.example.maps.R;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+
+public class MainActivity extends MapActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        MapView map = new MapView(this, "0jk9toza2GMaBQEe--jPaMvm_2g17l0hP_xnXfw");
+        setContentView(map);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
