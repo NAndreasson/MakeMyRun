4280c95e0b0329873744e88af3467cfb66a86f11
Anton Lindgren <lindgren.anton@gmail.com>
2012-09-25 15:36:45 +0200

    Testing to contact google with default query and parse it as JSON.

--- src/com/pifive/makemyrun/MainActivity.java
+++ src/com/pifive/makemyrun/MainActivity.java
@@ -1,6 +1,11 @@
 package com.pifive.makemyrun;
 
+import java.util.concurrent.ExecutionException;
+
+import org.json.JSONObject;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 
 import com.google.android.maps.MapActivity;
@@ -14,6 +19,19 @@ public class MainActivity extends MapActivity {
         
         MapView map = new MapView(this, "0jk9toza2GMaBQEe--jPaMvm_2g17l0hP_xnXfw");
         setContentView(map);
+        
+        GenerateRouteTask routeTask = new GenerateRouteTask();
+        try {
+        	routeTask.execute();
+        	JSONObject route = routeTask.get();
+        
+        	Log.i("Dev", route.toString());
+        	
+        } catch (ExecutionException e) {
+        	e.printStackTrace();
+        } catch (InterruptedException e) {
+        	e.printStackTrace();
+        }
     }
 
     @Override
