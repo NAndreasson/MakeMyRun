da31adb9c14793f9716bd2104820d55b481e8c9b
Victor Lindhé <victorlindhe@gmail.com>
2012-09-28 20:08:58 +0200

    circle generation method added

--- MakeMyRunTest/src/com/pifive/makemyrun/test/RouteGeneratorTest.java
+++ MakeMyRunTest/src/com/pifive/makemyrun/test/RouteGeneratorTest.java
@@ -1,15 +1,16 @@
 package com.pifive.makemyrun.test;
 
+import java.util.Iterator;
+import java.util.List;
+
+import org.junit.Test;
+
 import junit.framework.TestCase;
 import android.app.Activity;
-import android.content.ContentProvider;
-import android.content.ContentResolver;
 import android.content.Context;
-import android.content.Intent;
 import android.location.Location;
-import android.test.IsolatedContext;
 
-import com.pifive.makemyrun.MainActivity;
+import com.pifive.makemyrun.PiLocation;
 import com.pifive.makemyrun.RouteGenerator;
 
 /**
@@ -46,5 +47,39 @@ public class RouteGeneratorTest extends TestCase {
 	public void testGetCurrentLocation() {
 		Location returnLocation = RouteGenerator.getCurrentLocation(getTestContext());
 		assert(returnLocation != null);
+	}
+	
+	/**
+	 * Passes if:
+	 * 2 ² points ² 10
+	 * Each returned value doesn't equal the one before
+	 * Number of returned PiLocations equals the number of points sent in
+	 */
+	@Test (expected=IllegalArgumentException.class)
+	public void testGetCircle() {
+		PiLocation center = new PiLocation(57.7000, 12.000);
+		PiLocation start = new PiLocation(57.6990, 11.990);
+		@SuppressWarnings("unused")
+		List<PiLocation> locationsOne = RouteGenerator.getCircle(center, start, -34); // Exception
+		List<PiLocation> locationsTwo = RouteGenerator.getCircle(center, start, 15); // Exception
+		
+		assert(locationsTwo == null); // Should be null, right?
+		
+		List<PiLocation> workingLocations = RouteGenerator.getCircle(center, start, 10);
+		
+		assert(workingLocations.size() == 10);
+		
+		PiLocation lastLocation = null;
+		PiLocation presentLocation = null;
+		Iterator<PiLocation> iterator = workingLocations.iterator();
+		while(iterator.hasNext()) {
+			if(lastLocation == null) {
+				lastLocation = iterator.next();
+			} else {
+				presentLocation = iterator.next();
+				assert(presentLocation.getLat() != lastLocation.getLat() && 
+						presentLocation.getLng() != lastLocation.getLng());
+			}
+		}
 	}
 }
