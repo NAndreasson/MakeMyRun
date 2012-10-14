2cbf21360f83e4bda9ddb71954cd0bdbe7c743e1
Victor Lindhé <victorlindhe@gmail.com>
2012-10-14 21:29:22 +0200

    Made RouteGeneratorTest.java fit the new RouteGenerator class. 87% coverage :-)

--- MakeMyRunTest/src/com/pifive/makemyrun/test/RouteGeneratorTest.java
+++ MakeMyRunTest/src/com/pifive/makemyrun/test/RouteGeneratorTest.java
@@ -40,7 +40,7 @@ public class RouteGeneratorTest extends AndroidTestCase {
 		
 		String routeWithQuery = RouteGenerator.generateRoute(pointOne, pointTwo);
 		assertNotNull(routeWithQuery);
-		assert(routeWithQuery.getClass() == String.class);
+		assertTrue(routeWithQuery.getClass() == String.class);
 	}
 	
 	/**
@@ -64,6 +64,53 @@ public class RouteGeneratorTest extends AndroidTestCase {
 		String destLat = otherHalf.substring(indexOfDestination+1, indexOfSecondComma);
 		String destLong = otherHalf.substring(indexOfSecondComma+1, indexOfSecondAnd);
 		
-		assert(originLat.equals(destLat) && originLong.equals(originLong));
+		assertTrue(originLat.equals(destLat) && originLong.equals(originLong));
+	}
+	
+	public void testDifferentRoutes() {
+		testStraight(new GeoPoint((int)(57.7 * 1E6), (int)(12 * 1E6)), 
+				new GeoPoint((int)(57.7 * 1E6), (int)(12.01 * 1E6)));
+		testStraight(new GeoPoint((int)(57.7 * 1E6), (int)(12 * 1E6)), 
+				new GeoPoint((int)(57.71 * 1E6), (int)(12 * 1E6)));
+		testStraight(new GeoPoint((int)(57.7 * 1E6), (int)(12 * 1E6)), 
+				new GeoPoint((int)(57.715 * 1E6), (int)(12.02 * 1E6)));
+		testStraight(new GeoPoint((int)(57.7 * 1E6), (int)(12 * 1E6)), 
+				new GeoPoint((int)(57.702 * 1E6), (int)(12.5 * 1E6)));
+	}
+	
+	/**
+	 * Passes if returned waypoints doesn't equal sent in locations
+	 */
+	private void testStraight(GeoPoint pointOne, GeoPoint pointTwo) {
+		String routeWithQuery = RouteGenerator.generateRoute(pointOne, pointTwo);
+		
+		String waypoints = routeWithQuery.substring(routeWithQuery.indexOf("optimize:true|") + 
+													 "optimize:true|".length());
+		String firstLat = waypoints.substring(0, waypoints.indexOf(","));
+		String firstLong = waypoints.substring(waypoints.indexOf(",")+1, waypoints.indexOf("|"));
+		
+		System.out.println(routeWithQuery);
+		
+		assertTrue("Verifies the first waypoint lat != long", !firstLat.equals(firstLong));
+		assertFalse("Verifies first waypoint is not the same as origin", 
+				String.valueOf(pointOne.getLatitudeE6() / 1E6).equals(firstLat) 
+				&& String.valueOf(pointOne.getLongitudeE6() / 1E6).equals(firstLong));
+		
+		assertFalse("Verifies first waypoint is not the same as destination", 
+				String.valueOf(pointTwo.getLatitudeE6() / 1E6).equals(firstLat) 
+				&& String.valueOf(pointTwo.getLongitudeE6() / 1E6).equals(firstLong));
+		
+		String waypoints2 = waypoints.substring(waypoints.indexOf("|")+1);
+		String secondLat = waypoints2.substring(0, waypoints2.indexOf(","));
+		String secondLong = waypoints2.substring(waypoints2.indexOf(",")+1, waypoints2.indexOf("&"));
+		
+		assertTrue("Verifies the first waypoint lat != long", !secondLat.equals(secondLong));
+		assertFalse("Verifies first waypoint is not the same as origin", 
+				String.valueOf(pointOne.getLatitudeE6() / 1E6).equals(secondLat) 
+				&& String.valueOf(pointOne.getLongitudeE6() / 1E6).equals(secondLong));
+		assertFalse("Verifies first waypoint is not the same as destination", 
+				String.valueOf(pointTwo.getLatitudeE6() / 1E6).equals(secondLat) 
+				&& String.valueOf(pointTwo.getLongitudeE6() / 1E6).equals(secondLong));
+		
 	}
 }
